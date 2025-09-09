dcaa96482d1d7526d41df596de85e4b7b5f37fae
Ibrahim El Othmani <141041774+ibrahimelothmani@users.noreply.github.com>
2025-09-09 10:27:53 +0100

    Refactor data processing to include data cleaning and display relevant columns

--- app.py
+++ app.py
@@ -2,3 +2,8 @@ import pandas as pd
 
 df = pd.read_csv("Expense_data_1.csv")
 print(df.head())
+
+# CLEAN THE DATA
+
+data = df[["Date", "Category", "Note", "Amount", "Income/Expense"]]
+print(data.head())
